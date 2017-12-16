aa88ad2daa68378fa8f6652cf8ef3df8da0eaef2
adibmuhamad <adibmuhamad48@gmail.com>
2017-12-16 08:25:07 +0700

    Driver Location

--- app/src/main/java/com/online/ojek/ojekonline/MainActivity.java
+++ app/src/main/java/com/online/ojek/ojekonline/MainActivity.java
@@ -1,13 +1,239 @@
 package com.online.ojek.ojekonline;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.online.ojek.ojekonline.Model.User;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import dmax.dialog.SpotsDialog;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnRegister;
+    RelativeLayout rootLayout;
+
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+        .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+        .setFontAttrId(R.attr.fontPath)
+        .build());
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        rootLayout = (RelativeLayout)findViewById(R.id.rootLayout);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterDialog();
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLoginDialog();
+            }
+        });
+    }
+
+    private void showLoginDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("MASUK");
+        dialog.setMessage("Please use email to login");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout_login = inflater.inflate(R.layout.layout_login,null);
+
+        final MaterialEditText editEmail = layout_login.findViewById(R.id.editEmail);
+        final MaterialEditText editPassword = layout_login.findViewById(R.id.editPassword);
+
+        dialog.setView(layout_login);
+
+        dialog.setPositiveButton("MASUK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+
+                        btnSignIn.setEnabled(false);
+
+
+                        if (TextUtils.isEmpty(editEmail.getText().toString())) {
+                            Snackbar.make(rootLayout, "Please enter email address", Snackbar.LENGTH_SHORT)
+                                    .show();
+                            return;
+                        }
+
+                        if (TextUtils.isEmpty(editPassword.getText().toString())) {
+                            Snackbar.make(rootLayout, "Please enter password", Snackbar.LENGTH_SHORT)
+                                    .show();
+                            return;
+                        }
+
+                        if (editPassword.getText().length() < 6) {
+                            Snackbar.make(rootLayout, "Password too short !!!", Snackbar.LENGTH_SHORT)
+                                    .show();
+                            return;
+                        }
+
+                        final SpotsDialog waitingDialog = new SpotsDialog(MainActivity.this);
+                        waitingDialog.show();
+
+                        auth.signInWithEmailAndPassword(editEmail.getText().toString(),editPassword.getText().toString())
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        waitingDialog.dismiss();
+                                        startActivity(new Intent(MainActivity.this, Welcome.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                waitingDialog.dismiss();
+                                Snackbar.make(rootLayout, "Failed "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                        .show();
+                                btnSignIn.setEnabled(true);
+                            }
+                        });
+
+                    }
+                });
+        dialog.setNegativeButton("BATAL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+
+
+        dialog.show();
+    }
+
+    private void showRegisterDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("DAFTAR");
+        dialog.setMessage("Please use email to register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout_register = inflater.inflate(R.layout.layout_register,null);
+
+        final MaterialEditText editEmail = layout_register.findViewById(R.id.editEmail);
+        final MaterialEditText editPassword = layout_register.findViewById(R.id.editPassword);
+        final MaterialEditText editName = layout_register.findViewById(R.id.editName);
+        final MaterialEditText editPhone = layout_register.findViewById(R.id.editPhone);
+
+        dialog.setView(layout_register);
+
+        dialog.setPositiveButton("DAFTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+                if(TextUtils.isEmpty(editEmail.getText().toString())){
+                    Snackbar.make(rootLayout, "Please enter email address", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(editPhone.getText().toString())){
+                    Snackbar.make(rootLayout, "Please enter phone number", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(editPassword.getText().toString())){
+                    Snackbar.make(rootLayout, "Please enter password", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                if(editPassword.getText().length() < 6){
+                    Snackbar.make(rootLayout, "Password too short !!!", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(editEmail.getText().toString(),editPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user =new User();
+                                user.setEmail(editEmail.getText().toString());
+                                user.setPassword(editPassword.getText().toString());
+                                user.setName(editName.getText().toString());
+                                user.setPhone(editPhone.getText().toString());
+
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout, "Register successfully", Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout, "Failed "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        });
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(rootLayout, "Failed "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+            }
+        });
+        dialog.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
--- app/src/main/java/com/online/ojek/ojekonline/Welcome.java
+++ app/src/main/java/com/online/ojek/ojekonline/Welcome.java
@@ -1,13 +1,271 @@
 package com.online.ojek.ojekonline;
 
-import android.support.v7.app.AppCompatActivity;
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
 
-public class Welcome extends AppCompatActivity {
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitch;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Welcome extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener
+        {
+
+    private GoogleMap mMap;
+    private static final int  MY_PERMISSION_REQUEST_CODE = 7000;
+    private static final int PLAY_SERVICE_RES_REQUEST = 7001;
+
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+    private static int UPDATE_INTERVAL = 5000;
+    private static int FATEST_INTERVAL =  3000;
+    private static int DISPLACEMENT = 10;
+
+    DatabaseReference drivers;
+    GeoFire geoFire;
+
+    Marker mCurrent;
+    MaterialAnimatedSwitch location_switch;
+    SupportMapFragment mapFragment;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        location_switch = (MaterialAnimatedSwitch)findViewById(R.id.location_switch);
+        location_switch.setOnCheckedChangeListener(new MaterialAnimatedSwitch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(boolean isOnline) {
+                if(isOnline){
+                    startLocationUpdate();
+                    displayLocation();
+                    Snackbar.make(mapFragment.getView(), "You are online", Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+                else
+                {
+                    stopLocationUpdate();
+                    mCurrent.remove();
+                    Snackbar.make(mapFragment.getView(), "You are offline", Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+
+        drivers = FirebaseDatabase.getInstance().getReference("Drivers");
+        geoFire = new GeoFire(drivers);
+        setUpLocation();
     }
-}
+
+            @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                switch (requestCode){
+                    case MY_PERMISSION_REQUEST_CODE:
+                        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                            if(checkPlayServices()){
+                                buildGoogleApiClient();
+                                createLocationRequest();
+                                if(location_switch.isChecked())
+                                    displayLocation();
+                            }
+                        }
+                }
+
+            }
+
+            private void setUpLocation() {
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    }, MY_PERMISSION_REQUEST_CODE);
+                }
+                else {
+                    if(checkPlayServices()){
+                        buildGoogleApiClient();
+                        createLocationRequest();
+                        if(location_switch.isChecked())
+                            displayLocation();
+                    }
+                }
+            }
+
+            private void createLocationRequest() {
+                mLocationRequest = new LocationRequest();
+                mLocationRequest.setFastestInterval(UPDATE_INTERVAL);
+                mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+            }
+
+            private void buildGoogleApiClient() {
+                mGoogleApiClient = new GoogleApiClient.Builder(this)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .addApi(LocationServices.API)
+                        .build();
+                mGoogleApiClient.connect();
+            }
+
+            private boolean checkPlayServices() {
+                int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+                if(resultCode != ConnectionResult.SUCCESS){
+                    if(GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+                        GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICE_RES_REQUEST).show();
+                    else{
+                        Toast.makeText(this, "This device is not supported", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    return false;
+                }
+                return true;
+            }
+
+            private void stopLocationUpdate() {
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+
+            }
+
+            private void displayLocation() {
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                if(mLastLocation != null){
+                    if(location_switch.isChecked()){
+                        final double latitude = mLastLocation.getLatitude();
+                        final double longitude = mLastLocation.getLongitude();
+
+                        geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(), new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
+                            @Override
+                            public void onComplete(String key, DatabaseError error) {
+                                if(mCurrent != null){
+                                    mCurrent.remove();
+                                }
+                                mCurrent = mMap.addMarker(new MarkerOptions()
+                                                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.motor))
+                                                                .position(new LatLng(latitude,longitude))
+                                                                .title("You"));
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude), 15.0f));
+                                rotateMaker(mCurrent, -360, mMap);
+                            }
+                        });
+                    }
+                }
+                else{
+                    Log.d("ERROR", "Cannot get your location ");
+                }
+
+            }
+
+            private void rotateMaker(final Marker mCurrent, final float i, GoogleMap mMap) {
+                final Handler handler = new Handler();
+                final long start = SystemClock.uptimeMillis();
+                final float startRotation = mCurrent.getRotation();
+                final long duration = 1500;
+
+                final Interpolator interpolator = new LinearInterpolator();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        long elapsed = SystemClock.uptimeMillis() - start;
+                        float t = interpolator.getInterpolation((float)elapsed/duration);
+                        float rot = t*i+(1-t)*startRotation;
+                        mCurrent.setRotation(-rot > 180?rot/2:rot);
+                        if(t<1.0){
+                            handler.postDelayed(this, 16);
+                        }
+                    }
+                });
+            }
+
+            private void startLocationUpdate() {
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (com.google.android.gms.location.LocationListener) this);
+            }
+
+
+            @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+    }
+
+
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+                displayLocation();
+                startLocationUpdate();
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+                mGoogleApiClient.connect();
+            }
+
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                mLastLocation = location;
+                displayLocation();
+            }
+        }
