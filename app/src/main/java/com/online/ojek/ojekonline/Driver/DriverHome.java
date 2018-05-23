02c49ea5b385935f95d22f4f9b6219056db21d8e
adibmuhamad <adibmuhamad48@gmail.com>
2018-05-23 09:01:30 +0700

    Change password

--- app/src/main/java/com/online/ojek/ojekonline/Driver/DriverHome.java
+++ app/src/main/java/com/online/ojek/ojekonline/Driver/DriverHome.java
@@ -2,6 +2,8 @@ package com.online.ojek.ojekonline.Driver;
 
 import android.Manifest;
 import android.animation.ValueAnimator;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -12,7 +14,9 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -54,6 +58,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.SquareCap;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -66,14 +75,18 @@ import com.online.ojek.ojekonline.Common.Common;
 import com.online.ojek.ojekonline.Model.Token;
 import com.online.ojek.ojekonline.R;
 import com.online.ojek.ojekonline.remote.IGoogleAPI;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import dmax.dialog.SpotsDialog;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -574,17 +587,95 @@ public class DriverHome extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_way_bil) {
 
+        }else if (id == R.id.nav_change_pwd) {
+            showDialogChangePassword();
+
         } else if (id == R.id.nav_help) {
 
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_sign_out) {
-
+            signOut();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showDialogChangePassword() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(DriverHome.this);
+        alertDialog.setTitle("CHANGE PASSWORD");
+        alertDialog.setMessage("Please fill all information");
+        LayoutInflater inflater = this.getLayoutInflater();
+        View layout_pwd = inflater.inflate(R.layout.layout_change_password,null);
+
+        final MaterialEditText editPassword = (MaterialEditText) layout_pwd.findViewById(R.id.editPassword);
+        final MaterialEditText editNewPassword = (MaterialEditText) layout_pwd.findViewById(R.id.editNewPassword);
+        final MaterialEditText editRepeatPassword = (MaterialEditText) layout_pwd.findViewById(R.id.editRepeatPassword);
+
+        alertDialog.setView(layout_pwd);
+        alertDialog.setPositiveButton("CHANGE PASSWORD", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                final android.app.AlertDialog waitingDialog = new SpotsDialog(DriverHome.this);
+                waitingDialog.show();
+
+                if(editNewPassword.getText().toString().equals(editRepeatPassword.getText().toString())){
+                    String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                    AuthCredential credential = EmailAuthProvider.getCredential(email, editPassword.getText().toString());
+                    FirebaseAuth.getInstance().getCurrentUser()
+                            .reauthenticate(credential)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        FirebaseAuth.getInstance().getCurrentUser()
+                                                .updatePassword(editRepeatPassword.getText().toString())
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if(task.isSuccessful()){
+                                                            Map<String,Object> password = new HashMap<>();
+                                                            password.put("password", editRepeatPassword.getText().toString());
+                                                            DatabaseReference driverInformation = FirebaseDatabase.getInstance().getReference(Common.user_driver_tbl);
+                                                            driverInformation.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                                    .updateChildren(password)
+                                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                        @Override
+                                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                                            if(task.isSuccessful())
+                                                                                Toast.makeText(DriverHome.this, "Password was changed", Toast.LENGTH_SHORT).show();
+                                                                            else
+                                                                                Toast.makeText(DriverHome.this, "Password was changed but not update to database", Toast.LENGTH_SHORT).show();
+                                                                            waitingDialog.dismiss();
+                                                                        }
+                                                                    });
+                                                        }else{
+                                                            Toast.makeText(DriverHome.this, "Password doesn't change", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                    }else{
+                                        waitingDialog.dismiss();
+                                        Toast.makeText(DriverHome.this, "Wrong old password", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }else{
+                    waitingDialog.dismiss();
+                    Toast.makeText(DriverHome.this, "Password doesn't match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(DriverHome.this,DriverMainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
